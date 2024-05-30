c0c92ed024ee332ce2ead3a12a0043296076db84
Michal Shelenberg <michalshelenberg@tutanota.com>
2024-05-30 14:43:24 +0200

    Style changes

--- README.md
+++ README.md
@@ -1,16 +1,3 @@
-# Videolearning VŠEM Watchmarker
-
 <p align="center">
-  <img src="https://lh3.googleusercontent.com/XrZ4LR6Umb6LWT5QYvJitgYjhA_Z4KWmd5ya68nCwZMNx_GtI0joFSc1LVQKNY1vrk_ULbFSD3_so_mSZVt94Ycf=s800-w800-h500" />
+  <img src="https://www.michalshelenberg.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fvsem-watchmarker.32ff9556.png&w=1080&q=75" />
 </p>
-
-Rozšíření automaticky označuje již zhlédnuté lekce a vizualizuje postup v online výukovém prostředí Videolearning VŠEM. S pomocí tohoto rozšíření budete mít přehled o tom, které lekce jste již dokončili a které ještě čekají.
-
-## Installation
-
-- [Chrome Web Store](https://chromewebstore.google.com/detail/videolearning-v%C5%A1em-watchm/jkccjafmkadkabnfjepicjocjgphidbc)
-- [Firefox Add-ons](https://addons.mozilla.org/en-US/firefox/addon/videolearning-v%C5%A1em-watchmarker/)
-
-## License
-
-[MPL-2.0](LICENSE)
--- watchmarker.js
+++ watchmarker.js
@@ -1,16 +1,12 @@
-// Retrieve the video progress data from local storage
 chrome.storage.local.get("videos", (result) => {
   let videos = result.videos || {};
 
-  // Get all link elements with the class "edittllect"
   let aElements = document.querySelectorAll("a.edittllect");
 
-  // Loop through the NodeList of link elements and append the video progress data to each link
   aElements.forEach((aElement) => {
     let url = aElement.href;
     let videoData = videos[url];
     if (videoData) {
-      // Format the video progress data as a time string
       let currentTime = videoData.currentTime;
       let hours = parseInt(currentTime / 3600);
       let minutes = parseInt((currentTime % 3600) / 60);
@@ -24,7 +20,6 @@ chrome.storage.local.get("videos", (result) => {
       }
       timeString += `${seconds.toString().padStart(2, "0")}`;
 
-      // Append the video progress data to the link element's text content
       let progressString =
         videoData.progress >= 100 ? "✅" : `⏸️ (${timeString})`;
       aElement.innerText = `${progressString} ${aElement.innerText}`;
